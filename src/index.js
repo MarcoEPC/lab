4fb4b3c9b1971b7e2af68bc2eb8459b4788bdc3f
MarcoEPC <marcosilva@epcarvalhais.org>
2024-04-03 23:10:39 +0100

    Introdução ao dockerfile

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express=required("express")
+const express=require("express")
 const app=express()
 
 app.get('/',(req,res)=>{
 	res.send("Olá mundo")
 })
 
-app.listen(8000,()=>{
+app.listen(3000,()=>{
 	console.log("Servidor ok")
 })
