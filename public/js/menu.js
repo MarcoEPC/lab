652b75bc2ac43215bdba096a788341cd9e888cec
edmaelandrade <2171@epcarvalhais.org>
2024-04-29 13:46:29 +0100

    Ajustes  no menu.

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,9 +1,28 @@
-         document.getElementById('btnmenu').addEventListener('click',function(){
-            document.getElementById('menu').style.display='block'
-            document.getElementById('main').style.display = 'none'
-        })
-        document.getElementById('btnfechar').addEventListener('click', function () {
-                document.getElementById('menu').style.display = 'none'
-                document.getElementById('main').style.display = 'block'
-            })
- 
+document.addEventListener('DOMContentLoaded', function() {
+    let btnMenu = document.getElementById('btnmenu');
+    let btnFechar = document.getElementById('btnfechar');
+    let menu = document.getElementById('menu');
+
+    function toggleMenu(isOpening) {
+        let animationName = isOpening ? 'slideIn 0.5s' : 'slideOut 0.5s';
+        menu.style.animation = animationName;
+        menu.classList.toggle('show', isOpening);
+        menu.classList.toggle('hide', !isOpening);
+        main.style.display = isOpening ? 'none' : 'block';
+        if (!isOpening) {
+            setTimeout(function() {
+                menu.style.display = 'none';
+            }, 500); 
+        }
+    }
+
+    btnMenu.addEventListener('click', function() {
+        menu.style.display = 'block';
+        toggleMenu(true);
+    });
+
+    btnFechar.addEventListener('click', function() {
+        toggleMenu(false);
+    });
+});
+
