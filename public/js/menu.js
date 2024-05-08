6e4e1d003a6affec82e38d710eddcf836fa3e8b0
edmaelandrade <2171@epcarvalhais.org>
2024-05-08 20:42:33 +0100

    Melhoria na funcionalidade do menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,28 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let btnMenu = document.getElementById('btnmenu');
-    let btnFechar = document.getElementById('btnfechar');
-    let menu = document.getElementById('menu');
-
-    function toggleMenu(isOpening) {
-        let animationName = isOpening ? 'slideIn 0.5s' : 'slideOut 0.5s';
-        menu.style.animation = animationName;
-        menu.classList.toggle('show', isOpening);
-        menu.classList.toggle('hide', !isOpening);
-        main.style.display = isOpening ? 'none' : 'block';
-        if (!isOpening) {
-            setTimeout(function() {
-                menu.style.display = 'none';
-            }, 500); 
-        }
-    }
-
-    btnMenu.addEventListener('click', function() {
-        menu.style.display = 'block';
-        toggleMenu(true);
-    });
-
-    btnFechar.addEventListener('click', function() {
-        toggleMenu(false);
+document.addEventListener('DOMContentLoaded', () => {
+    ['btnmenu', 'btnfechar'].forEach((id, i) => {
+        document.getElementById(id).addEventListener('click', () => {
+            let isOpening = i === 0;
+            document.getElementById('menu').style.display = isOpening ? 'block' : 'none';
+            if (document.getElementById('main')) {
+                document.getElementById('main').style.display = isOpening ? 'none' : 'block';
+            }
+        });
     });
 });
 
